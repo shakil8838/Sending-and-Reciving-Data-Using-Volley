edd159c69ce52351d58a51cfdfbdc07f5c2ef80f
Md Shakil Ahmed <eng.shakil8838@gmail.com>
2018-05-05 21:52:36 +0600

    data loding issu fixed

--- app/src/main/java/com/example/sendingandrecivingdatausingvolley/MainActivity.java
+++ app/src/main/java/com/example/sendingandrecivingdatausingvolley/MainActivity.java
@@ -15,19 +15,31 @@ import com.example.sendingandrecivingdatausingvolley.adapters.RecyclerAdapter;
 import com.example.sendingandrecivingdatausingvolley.apis.BackgroundTask;
 import com.example.sendingandrecivingdatausingvolley.dataProviders.FetchDataProvider;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView recyclerView;
     private RecyclerView.LayoutManager layoutManager;
-    private List<FetchDataProvider> list;
+    private List<FetchDataProvider> list = new ArrayList<FetchDataProvider>();
     private RecyclerAdapter recyclerAdapter;
     private FloatingActionButton floatingActionButton;
+
+    private static MainActivity instance = null;
+
+    public static MainActivity getInstance(){
+        return instance;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        instance = MainActivity.this;
         setContentView(R.layout.activity_main);
 
         floatingActionButton = (FloatingActionButton) findViewById(R.id.AddItem);
@@ -44,10 +56,29 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         BackgroundTask backgroundTask = new BackgroundTask(MainActivity.this);
-        list = backgroundTask.getArrayList();
-        recyclerAdapter = new RecyclerAdapter(list,MainActivity.this);
+        backgroundTask.getArrayList();
+        recyclerAdapter = new RecyclerAdapter(list, MainActivity.this);
         recyclerView.setAdapter(recyclerAdapter);
 
+    }
+
+    public void parseJsonData(JSONArray response){
+        int count = 0;
+        while (count < response.length()) {
+
+            try {
+                JSONObject jsonObject = response.getJSONObject(count);
+                FetchDataProvider fetchDataProvider = new FetchDataProvider(
+                        jsonObject.getString("Date"),
+                        jsonObject.getString("Time"),
+                        jsonObject.getString("Details"));
+                list.add(fetchDataProvider);
+                count++;
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        recyclerAdapter.notifyDataSetChanged();
     }
 
     public boolean isNetWorkAvailable(){
--- app/src/main/java/com/example/sendingandrecivingdatausingvolley/apis/BackgroundTask.java
+++ app/src/main/java/com/example/sendingandrecivingdatausingvolley/apis/BackgroundTask.java
@@ -6,6 +6,7 @@ import com.android.volley.Request;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
+import com.example.sendingandrecivingdatausingvolley.MainActivity;
 import com.example.sendingandrecivingdatausingvolley.dataProviders.FetchDataProvider;
 import com.example.sendingandrecivingdatausingvolley.dataProviders.SettingUpData;
 
@@ -21,34 +22,18 @@ import java.util.ArrayList;
 public class BackgroundTask {
 
     Context context;
-    ArrayList<FetchDataProvider> arrayList = new ArrayList<FetchDataProvider>();
 
     public BackgroundTask(Context context) {
         this.context = context;
     }
 
-    public ArrayList<FetchDataProvider> getArrayList() {
+    public void getArrayList() {
 
         JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, URLs.FETCH_DATA_URL, null,
                 new Response.Listener<JSONArray>() {
                     @Override
                     public void onResponse(JSONArray response) {
-
-                        int count = 0;
-                        while (count < response.length()) {
-
-                            try {
-                                JSONObject jsonObject = response.getJSONObject(count);
-                                FetchDataProvider fetchDataProvider = new FetchDataProvider(
-                                        jsonObject.getString("Date"),
-                                        jsonObject.getString("Time"),
-                                        jsonObject.getString("Details"));
-                                arrayList.add(fetchDataProvider);
-                                count++;
-                            } catch (JSONException e) {
-                                e.printStackTrace();
-                            }
-                        }
+                        MainActivity.getInstance().parseJsonData(response);
                     }
                 },
                 new Response.ErrorListener() {
@@ -60,6 +45,6 @@ public class BackgroundTask {
 
         SettingUpData.getIntance(context).addToReequestQue(jsonArrayRequest);
 
-        return arrayList;
+//        return arrayList;
     }
 }
